4be0a632eb9b4936ea7e9b2d7329c4f65d7874d9
Adriel Ryan <adrielryan.tj@hotmail.com>
2022-09-11 08:34:43 -0300

    other  components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+  return (
+    <div className="dsmeta-logo-container">
+      <img src={logo} alt="logo da loja DSMeta" />
+      <h1>DSMeta</h1> 
+      <p>
+        Desenvolvido por
+        <a href="https://www.linkedin.com/in/adriel-ryan/">Adriel Ryan</a>
+      </p>
+    </div>
+  );
+}
+
+export default Header;
--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -4,11 +4,13 @@ import './styles.css'
 
 function NotificationButton() {
   return (
+    <div className="dsmeta-red-btn-container">
     <div className="dsmeta-red-btn">
     <img
       src={icon}
       alt="notificar"
     />
+  </div>
   </div>
   );
 }
